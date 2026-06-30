ac8453e864d599b78ecd97b5195b51dc90a7321a
vijayitapp <vijayitapp@gmail.com>
2026-06-30 18:42:04 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Madurai-Cricket-Veerargal/sw.js', { scope: '/Madurai-Cricket-Veerargal/' })})}
